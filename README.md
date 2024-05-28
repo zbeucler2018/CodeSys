b36ffea3add375ca8a5f917447dfa1f27fc5d9ff
e71828 <740943170@qq.com>
2024-05-28 16:45:33 +0800

    load source

--- README.md
+++ README.md
@@ -6,16 +6,24 @@
 
 ## 脚本说明:
 
-`Export.py`:
+`export.py`:
 
 - [x] 将 Codesys 内的 ST 语言的文本代码 和 Global_var, Textlist 和 Task_Configuration, library 备份到 Save_Folder 文件夹。
 - [x] 将指定的文件夹内的 文本代码 备份到 Save_Folder 文件夹，不指定则为工况全部的文本代码。
 - [x] 若 Save_Folder 文件夹 非空，则提示要删除其中文件，但不包括 `.git` 与 `.svn` 文件。
-- [ ] 如果文件夹内存在.git 文件,则将文件夹更新到HEAD.
+- [ ] 如果文件夹内存在 .git 文件,则将文件夹更新到 HEAD 。
   
-`Load.py`:
-- [ ] 将上述文件夹内文本代码的导入到当前工程。
+`load.py`:
+- [x] 将上述文件夹内文本代码的导入到当前工程。
+- [ ] 设备文件夹/文件导入。
   
-问题:
-  - 除ST语言的文本外,其他如: Visu,imagePool, VisuConfiguration, Project Settings, Project Infomation 没有导出.
-  - GlobalTextList 会丢失 ID Column 的数据.
+## 问题:
+
+- 除ST语言的文本外,其他如: Visu,imagePool, VisuConfiguration, Project Settings, Project Infomation 没有导出.
+- GlobalTextList 会丢失 ID Column 的数据.
+
+
+## 致谢:
+
+- [`CODESYS Online Help`](https://help.codesys.com/webapp/System;product=ScriptEngine)
+- [`18thCentury/CodeSys`](https://github.com/18thCentury/CodeSys)
--- load.py
+++ load.py
@@ -1,322 +1,267 @@
+#!/bin/env python3
 # encoding:utf-8
 from __future__ import print_function
 import os
 import shutil
-PROJECT = r"D:\project\CoDeSys\HMI\a.project"
+import clr
+from System import Environment
 
-from_folder=r'D:\Gitlab\codesys\Yao'
+clr.AddReference('System.Windows.Forms')
+from System.Windows.Forms import FolderBrowserDialog, DialogResult
 
 
 def check(func):
-    def wrapper(proj,path,name):  # 指定宇宙无敌参数
-		#call func
-		func(proj,path,name)
-		#check
-		name=name.split(".")[0]
-		found = proj.find(name, False)
-		assert(found is not None and len(found) == 1, 'No object with the name {0} found '.format(name))
-		#assert(found[0].is_folder, 'Found object is not a folder')
-		item = found[0]
-		return item
+    def wrapper(proj, path, name):  # 指定宇宙无敌参数
+        # call func
+        func(proj, path, name)
+        # check
+        found = proj.find(name, False)
+        assert (found is not None and len(found) == 1, 'No object with the name {0} found '.format(name))
+        item = found[0]
+        return item
+
     return wrapper  # 返回
 
 
-def insert_text(proj,path,name):
-	with open(path,'r') as f:
-		text = f.read()
-		index = text.find('(*#-#-#-#-#-#-#-#-#-#---Implementation---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n')
-		
-		if index >=0:
-			t1= text[index:].replace('(*#-#-#-#-#-#-#-#-#-#---Implementation---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n','')
-			proj.textual_implementation.replace(t1)
-			#print(name+"*** t1")
-			try:
-				t1= text[:index].replace('(*#-#-#-#-#-#-#-#-#-#---Declaration---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n','')
-				proj.textual_declaration.replace(t1)
-			except:
-				pass
-			
-		else:
-			t1= text.replace('(*#-#-#-#-#-#-#-#-#-#---Declaration---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n','')
-			proj.textual_declaration.replace(t1)
-
-	
-def export_visu(projects):
-	proj = projects.primary
-	device = proj.find('_3S_Testspec_Device')[0]
-	device.export_xml(reporter, ExportFileName, recursive = True)
-
-def create_taskconfig(proj,path,name):
-	proj = proj.create_task_configuration()
-	return proj
-	#projects.primary.import_native(path)
-	
-	
-def create_task(proj,path,name):
-	name=name.split('.')[0]
-	proj.import_native(path)
-	
-	
-def create_task_old(proj,path,name):
-	name=name.split('.')[0]
-	proj=proj.create_task(name)
-	
-	pou_names=[]
-	interval=''
-	priority=''
-	kindOftask=''
-	try:
-
-		with open(path,'r') as f:
-			t=f.readline()
-			pou_names=t.split("=")[1].replace("\r","").replace("\n","").split(',')
-			t=f.readline()
-			interval=t.split("=")[1].replace("\r","").replace("\n","")
-			t=f.readline()
-			kindOftask=t.split("=")[1].replace("\r","").replace("\n","")
-	except:
-		system.ui.info('open file:\n{0} \nfailed!'.format(path))
-		return
-	try:
-		proj.interval=interval
-		proj.priority=priority
-	
-		for i in pou_names:
-			if i:
-				try:
-					proj.pous.add(i)
-				except:
-					pass
-	
-		proj.kind_of_task= kindOftask		
-	except:
-		pass
-		
-		
-@check		
-def create_dev(proj,path,name):
-	type=0
-	id=''
-	ver=''
-	
-	with open(os.path.join(path,name),'r') as f:
-		type=f.readline()
-		type=int(type.split("=")[1].replace("\r\n",""))
-		id=f.readline()
-		id=id.split("=")[1].replace("\r","").replace("\n","")
-		ver=f.readline()
-		ver=ver.split("=")[1].replace("\r","").replace("\n","")
-		
-	#system.ui.info("create device:\ntype:{0}\nid:{1}\nver:{2}".format(type,id,ver))
-	
-	devId = device_repository.create_device_identification(type, id, ver)
-	devDesc = device_repository.get_device(devId)
-	
-	if devDesc is None:
-		system.ui.info("create device ERR:\n{0}\n{1}\n{2}".format(type,id,ver))
-		raise Exception('No WinV3 PLC available in device repo')
-	
-	name=name.split('.')[0]
-	# Add PLC to an empty project using a DeviceId instance
-	proj.add(name, devId)
+def insert_text(proj, path, name):
+    with open(path, 'r') as f:
+        text = f.read()
+        index = text.find('(*#-#-#-#-#-#-#-#-#-#---Implementation---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n')
+        if index >= 0:
+            t1 = text[index:].replace('(*#-#-#-#-#-#-#-#-#-#---Implementation---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n', '')
+            proj.textual_implementation.replace(t1)
+            try:
+                t1 = text[:index].replace('(*#-#-#-#-#-#-#-#-#-#---Declaration---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n', '')
+                proj.textual_declaration.replace(t1)
+            except:
+                pass
+        else:
+            t1 = text.replace('(*#-#-#-#-#-#-#-#-#-#---Declaration---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n', '')
+            proj.textual_declaration.replace(t1)
 
 
+def create_taskconfig(proj, path, name):
+    proj = proj.create_task_configuration()
+    return proj
 
-def create_app(proj,path,name):
-	pass
-	
-@check	
-def create_pou(proj,path,name):
-	name=name.split('.')[0]
-	proj.create_pou(name, PouType.Program)
-	
-@check	
-def create_gvl(proj,path,name):
-	name=name.split(".")[0]
-	proj.create_gvl(name)
-	
-@check
-def create_property(proj,path,name):
-	name=name.split(".")[0]
-	proj.create_property(name)
-	
 
-def create_method(proj,path,name):
-	name=name.split(".")[0]
-	proj = proj.create_method(name)
-	return proj
-	
-def create_act(proj,path,name):
-	name=name.split(".")[0]
-	try:
-		proj = proj.create_action(name)
-	except:
-		system.ui.info("create action:{0}\nfailed".format(name))
-			
-	return proj
+def create_task(proj, path, name):
+    proj.import_native(path)
+
 
 @check
-def create_folder(proj,path,name):
-	proj.create_folder(name)
+def create_dev(proj, path, name):
+    with open(path, 'r') as f:
+        type = int(f.readline().split("=")[1].strip())
+        id = f.readline().split("=")[1].strip()
+        ver = f.readline().split("=")[1].strip()
+    devId = device_repository.create_device_identification(type, id, ver)
+    devDesc = device_repository.get_device(devId)
+    if devDesc is None:
+        raise Exception('No WinV3 PLC available in device repo')
+    proj.add(name, devId)
 
-@check	
-def create_fb(proj,path,name):
-	name=name.split('.')[0]
-	proj.create_pou(name, PouType.FunctionBlock)
-	
+
 @check
-def create_fuction(proj,path,name):
-	name=name.split(".")[0]
-	proj.create_pou(name,PouType.Function)
-
-@check	
-def create_itf(proj,path,name):
-	name=name.split(".")[0]
-	proj.create_interface(name)
-	found = proj.find(name, False)
-	
-	
-def create_dut(proj,path,name):
-	name=name.split(".")[0]
-	item=proj.create_dut(name,DutType.Union)
-	
-	return item
-	
-def add_library(proj,path,name):
-	name=name.split(".")[0]
-	#system.ui.info("add library:{0}".format(name))
-	proj.import_native(path)
-	
-
-def add_textlist(proj,path,name):
-	name=name.split(".")[0]
-	try:
-		proj=proj.create_textlist(name)
-		proj.importfile(path)
-		
-	except:
-		pass
-		
-@check	
-def add_prop_method(proj,path,name):
-	name = name.split(".")[0]
-	
-	
-	
-def walk_folder(proj,path,tp):
-	
-	curpath=path
-	
-	for fi in os.listdir(curpath):
-		sub_path = os.path.join(curpath,fi)
-		is_folder = os.path.isdir(sub_path)
-		stp= fi.split('.')
-		
-		fn=stp[0]
-		try :
-			stp=stp[1]
-		except:
-			stp=''
-			
-		if tp!='' and tp==stp and not is_folder:
-			insert_text(proj,sub_path,fi)
-		else:
-			if stp=='dev':
-
-				sub_proj= create_dev(proj,sub_path,fi)
-				sub_path=os.path.join(sub_path,'Plc Logic')
-				sub_proj=sub_proj.find('Plc Logic',False)[0]
-				if not sub_proj :
-					raise Exception('No PLC Logic in device')
-				
-				for sfi in os.listdir(sub_path):
-					sub_sub_path= os.path.join(sub_path,sfi)
-					sub_sub_proj=sub_proj.find(sfi,False)[0]
-					if sub_sub_proj :
-						walk_folder(sub_sub_proj,sub_sub_path,'app')
-					else:
-						sub_sub_proj = create_app(sub_sub_proj,sub_sub_path,sfi)
-						walk_folder(sub_sub_proj,sub_sub_path,'app')
-			
-			elif stp=='pou':
-				sub_proj= create_pou(proj,sub_path,fi)
-				if not is_folder:
-					insert_text(sub_proj,sub_path,fi)
-				else:
-					walk_folder(sub_proj,sub_path,'pou')
-
-			elif stp=='itf':
-				sub_proj= create_itf(proj,sub_path,fi)
-				if not is_folder:
-					insert_text(sub_proj,sub_path,fi)
-				else:
-					walk_folder(sub_proj,sub_path,'itf')
-			
-			elif stp=='gvl' : #gvl resistant
-				sub_proj=create_gvl(proj,sub_path,fi)
-				#system.ui.info("GVL:"+sub_path)
-				insert_text(sub_proj,sub_path,fi)
-				
-			elif stp=='prop':
-				sub_proj=create_property(proj,sub_path,fi)
-				if not is_folder:
-					insert_text(sub_proj,sub_path,fi)
-				else:
-					walk_folder(sub_proj,sub_path,'prop')
-					
-			elif stp=='pm': #property method
-				sub_proj = add_prop_method(proj,sub_path,fi)
-				if not is_folder:
-					insert_text(sub_proj,sub_path,fi)
-				else:
-					walk_folder(sub_proj,sub_path,'pm')
-				
-			elif stp=='m': #method 
-				sub_proj=create_method(proj,sub_path,fi)
-				if not is_folder:
-					insert_text(sub_proj,sub_path,fi)
-				else:
-					walk_folder(sub_proj,sub_path,'m')
-					
-			elif stp=='act': #action 
-				sub_proj=create_act(proj,sub_path,fi)
-				if not is_folder:
-					insert_text(sub_proj,sub_path,fi)
-				else:
-					walk_folder(sub_proj,sub_path,'act')
-					
-			elif stp=='dut':
-				sub_proj=create_dut(proj,sub_path,fi)
-				insert_text(sub_proj,sub_path,fi)
-			
-			elif stp=='tc': #task configuration
-				sub_proj=create_taskconfig(proj,sub_path,fi)
-				if is_folder:
-					walk_folder(sub_proj,sub_path,'tc')
-			elif stp=='task':
-				create_task(proj,sub_path,fi)
-			
-			elif stp=='': #folder
-				sub_proj=create_folder(proj,sub_path,fi)
-				walk_folder(sub_proj,sub_path,tp)
-				
-			elif stp=='lib':
-				add_library(proj,sub_path,fi)
-				
-			elif stp=='tl': # textlist
-				add_textlist(proj,sub_path,fi)
-				
-			elif stp=='gtl': #global_textlist
-				add_textlist(proj,sub_path,fi)
-			else:
-				pass
-
-if projects.primary:
-	projects.primary.close()
-
-proj = projects.create(PROJECT)
-
-walk_folder(proj,from_folder,'')
+def create_pou(proj, path, name):
+    proj.create_pou(name, PouType.Program)
 
 
-system.ui.info("ok")
+@check
+def create_gvl(proj, path, name):
+    proj.create_gvl(name)
+
+
+@check
+def create_property(proj, path, name):
+    proj.create_property(name)
+
+
+def create_method(proj, path, name):
+    proj = proj.create_method(name)
+    return proj
+
+
+def create_act(proj, path, name):
+    try:
+        proj = proj.create_action(name)
+    except:
+        pass
+    return proj
+
+
+@check
+def create_folder(proj, path, name):
+    proj.create_folder(name)
+
+
+@check
+def create_fb(proj, path, name):
+    proj.create_pou(name, PouType.FunctionBlock)
+
+
+@check
+def create_function(proj, path, name):
+    proj.create_pou(name, PouType.Function)
+
+
+@check
+def create_itf(proj, path, name):
+    proj.create_interface(name)
+
+
+def create_dut(proj, path, name):
+    item = proj.create_dut(name, DutType.Union)
+    return item
+
+
+def add_library(proj, path, name):
+    proj.import_native(path)
+
+
+def add_textlist(proj, path, name):
+    try:
+        proj = proj.find(name, False)[0]
+    except ValueError:
+        proj = proj.create_textlist()
+    proj.importfile(path)
+
+
+@check
+def add_prop_method(proj, path, name):
+    pass
+
+
+def walk_folder(proj, path, depth=0):
+    for f_name in os.listdir(path):
+        sub_path = os.path.join(path, f_name)
+        is_folder = os.path.isdir(sub_path)
+        try:
+            name, f_ext = f_name.split('.')  # File extension
+        except ValueError:
+            name = f_name
+            f_ext = ''
+
+        if depth == 0:
+            print('main device: {}'.format(name))
+            try:
+                dev_proj = proj.find(name, False)[0]
+            except ValueError:
+                return False
+            logic_path = os.path.join(sub_path, 'Plc Logic')  # Corrected the path construction
+            sub_proj = dev_proj.find('Plc Logic', False)[0]
+            sub_path = os.path.join(logic_path, 'Application')  # Corrected the path construction
+            sub_proj = sub_proj.find('Application', False)[0]
+            walk_folder(sub_proj, sub_path, depth + 3)
+        else:
+            print('current project directory:    ', proj.get_name())
+            if is_folder:
+                print('input folder: {}{}'.format(' ' * 4, name))
+                if f_ext == 'tc':
+                    try:
+                        sub_proj = proj.find(name, False)[0]
+                    except ValueError:
+                        sub_proj = create_taskconfig(proj, sub_path, name)
+                    walk_folder(sub_proj, sub_path, depth + 1)
+                elif f_ext == '':
+                    try:
+                        sub_proj = proj.find(name, False)[0]
+                    except ValueError:
+                        sub_proj = create_folder(proj, sub_path, name)
+                    walk_folder(sub_proj, sub_path, depth + 1)
+                else:
+                    pass
+            else:
+                print('input file: {}{}.{}'.format(' ' * 40, name, f_ext))
+                if f_ext == 'pou':
+                    try:
+                        sub_proj = proj.find(name, False)[0]
+                    except ValueError:
+                        sub_proj = create_pou(proj, sub_path, name)
+                    insert_text(sub_proj, sub_path, name)
+                elif f_ext == 'itf':
+                    try:
+                        sub_proj = proj.find(name, False)[0]
+                    except ValueError:
+                        sub_proj = create_itf(proj, sub_path, name)
+                    insert_text(sub_proj, sub_path, name)
+                elif f_ext == 'gvl':
+                    try:
+                        sub_proj = proj.find(name, False)[0]
+                    except ValueError:
+                        sub_proj = create_gvl(proj, sub_path, name)
+                    insert_text(sub_proj, sub_path, name)
+                elif f_ext == 'prop':
+                    try:
+                        sub_proj = proj.find(name, False)[0]
+                    except ValueError:
+                        sub_proj = create_property(proj, sub_path, name)
+                    insert_text(sub_proj, sub_path, name)
+                elif f_ext == 'pm':
+                    try:
+                        sub_proj = proj.find(name, False)[0]
+                    except ValueError:
+                        sub_proj = add_prop_method(proj, sub_path, name)
+                    insert_text(sub_proj, sub_path, name)
+                elif f_ext == 'm':
+                    try:
+                        sub_proj = proj.find(name, False)[0]
+                    except ValueError:
+                        sub_proj = create_method(proj, sub_path, name)
+                    insert_text(sub_proj, sub_path, name)
+                elif f_ext == 'act':
+                    try:
+                        sub_proj = proj.find(name, False)[0]
+                    except ValueError:
+                        sub_proj = create_act(proj, sub_path, name)
+                    insert_text(sub_proj, sub_path, name)
+                elif f_ext == 'dut':
+                    try:
+                        sub_proj = proj.find(name, False)[0]
+                    except ValueError:
+                        sub_proj = create_dut(proj, sub_path, name)
+                    insert_text(sub_proj, sub_path, name)
+                elif f_ext == 'task':
+                    create_task(proj, sub_path, name)
+                elif f_ext == 'lib':
+                    add_library(proj, sub_path, name)
+                elif f_ext == 'tl':
+                    add_textlist(proj, sub_path, name)
+                elif f_ext == 'gtl':
+                    add_textlist(proj, sub_path, name)
+                else:
+                    pass
+    return True
+
+
+def search_folder():
+    # 获取 MyDocuments 路径
+    root = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+    # 目标路径 CsysL
+    default_path = os.path.join(root, 'CsysL')
+    if not os.path.exists(default_path): default_path = os.path.join(root)
+
+    def browse_directory_dialog(description, selected_path):
+        dialog = FolderBrowserDialog()
+        dialog.Description = description
+        dialog.SelectedPath = selected_path  # 设置默认路径
+        dialog.ShowNewFolderButton = True
+
+        if dialog.ShowDialog() == DialogResult.OK:
+            return dialog.SelectedPath
+        return None
+
+    has_repo = False  # git
+    # 使用 browse_directory_dialog 函数
+    selected_path = browse_directory_dialog("Choose a directory？ 简: 选择源代码位置。   ", default_path)
+    print("Nice, you choose: '%s'" % selected_path)
+    return selected_path
+
+if __name__ == '__main__':
+    source_folder = search_folder()
+    if source_folder:
+        status = walk_folder(projects.primary, source_folder)
+        if status:
+            system.ui.info("status")
+        else:
+            system.ui.warning("something went wrong")
